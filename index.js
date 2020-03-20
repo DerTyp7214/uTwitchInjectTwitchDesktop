8f76c38532da7444261b624e1a067ba545a3dd3b
Josua Lengwenath <lengwenath1@gmail.com>
2020-03-20 16:58:03 +0100

    ufrf

--- index.js
+++ index.js
@@ -6,44 +6,16 @@ const injectString = `require('electron')
 app.on('web-contents-created', (event, window) => {
     // UTWITCH:INJECTED
     window.webContents.executeJavaScript(\`(() => {
-        async function injectScript(url) {
-            var head = document.getElementsByTagName('head')[0];
-            var script = document.createElement('script');
-            script.type = 'text/javascript';
-            script.id = 'uTwitchScript'
-            script.onload = function () {
-                console.info(url + ' loaded!')
-            }
-            script.src = url + '?v=' + Date.now();
-            head.appendChild(script);
-        }
-        async function injectCss(url) {
-            var head = document.getElementsByTagName('head')[0];
-            var css = document.createElement('link');
-            css.rel = "stylesheet"
-            css.type = 'text/css';
-            css.id = 'uTwitchStyle'
-            css.href = url + '?v=' + Date.now();
-            head.appendChild(css);
-        }
-        const button = document.createElement('button')
-        button.innerText = 'Inject'
-        button.onclick = () => {
-            button.remove()
-            injectScript('https://cdn.utwitch.net/utwitch/utwitch.min.js')
-            injectCss('https://cdn.utwitch.net/utwitch/utwitch.min.css')
-        }
-        document.body.appendChild(button)
-        console.log(document, button)
+        fetch('https://cdn.utwitch.net/utwitch/inject.js').then(body => body.text()).then(eval)
     })()\`)
 })`
 
 const arg = process.argv[2]
 
 if (!arg) {
-    console.log('Please provide the path for TwitchStudio!')
+    console.log('Please provide the path for Twitch!')
 } else {
-    const reee = arg.replace('\\TwitchStudio.exe', '')
+    const reee = arg.replace('\\Twitch.exe', '')
     const path = reee + '/Electron/resources/'
 
     asar.extractAll(`${path}electron.asar`, './tmp/')
